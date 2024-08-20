30b7439552f5a331fd46181cb42ae7d1556c1951
chalacols <talltonchalacoeng@gmail.com>
2024-08-20 17:33:03 +0000

    quasar mode

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,18 @@
 <template>
   <q-page class="flex flex-center">
+    <p>Current Quasar Mode: {{ quasarMode }}</p>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script setup>
+import { computed } from "vue";
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
+const quasarMode = computed(() => process.env.MODE);
 </script>
