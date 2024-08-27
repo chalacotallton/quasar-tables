cb2e20d0d06fdb61a5eda45fdda0dc3f64b34ba0
chalacols <talltonchalacoeng@gmail.com>
2024-08-27 12:46:29 +0000

    asd

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page class="row justify-start items-start content-start">
+    <div col-3> {{ dbs }} {{ selection }}</div>
     <div class="q-gutter-sm col-3">
       <q-checkbox
         v-for="db in dbs"
